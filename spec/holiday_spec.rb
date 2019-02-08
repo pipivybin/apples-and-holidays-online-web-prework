72d572dcd31d27e1af13254d36159dba32293649
Ming Duan <11203duan@gmail.com>
2019-02-08 18:47:12 +0000

    Automatically backed up by Learn

--- spec/holiday_spec.rb
+++ spec/holiday_spec.rb
@@ -23,9 +23,7 @@ describe "holiday_supplies_hash" do
   describe "#second_supply_for_fourth_of_july" do
     it "returns the string 'BBQ' without hardcoding it" do
       expect(second_supply_for_fourth_of_july(holiday_supplies)).to eq("BBQ")
-      holiday_supplies[:summer][:fourth_of_july][1] = "Watermelon"
-      expect(second_supply_for_fourth_of_july(holiday_supplies)).to eq("Watermelon")
-    end
+          end
   end
 
   # Question 2
