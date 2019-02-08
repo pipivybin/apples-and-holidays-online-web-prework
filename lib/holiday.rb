63392512547c89b2242a6faaf37f08fa35d6774c
Ming Duan <11203duan@gmail.com>
2019-02-08 21:46:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,8 +58,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+result = []
 holiday_hash.each {
-  |season, hash| result = []
+  |season, hash|
     hash.each {
       |holiday, supplies| if supplies.any?("BBQ")
       result << holiday
