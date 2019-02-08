cfd55d05fd467ea4c9d3b53cdd0fca6e92716e8d
Ming Duan <11203duan@gmail.com>
2019-02-08 21:49:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,8 +56,6 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
 result = []
 holiday_hash.each {
   |season, hash|
