ec2b7966f99c1fa861427609254d9975b234c970
Ming Duan <11203duan@gmail.com>
2019-02-08 20:26:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ holiday_hash.each {
       nam.to_s.split(" ").each {
         |word| capitalized_w = word.capitalize
         nam_new << capitalized_w}
-      final = nam_new + [": "] + supp.to_s.capitalize
+      final = nam_new + [: ] + supp.to_s.capitalize
 
       puts "final.flatten"
     }
